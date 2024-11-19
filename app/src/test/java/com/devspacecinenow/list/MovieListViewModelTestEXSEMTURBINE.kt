14250ea932077ec904a77ab5a341155a94933263
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-11-19 07:02:16 -0300

    Implementando testes detalhadamente

---
+++ app/src/test/java/com/devspacecinenow/list/MovieListViewModelTestEXSEMTURBINE.kt
@@ -0,0 +1,67 @@
+package com.devspacecinenow.list
+
+import com.devspacecinenow.common.data.local.MovieCategory
+import com.devspacecinenow.common.data.model.Movie
+import com.devspacecinenow.list.data.MovieListRepository
+import com.devspacecinenow.list.presentation.MovieListViewModel
+import com.devspacecinenow.list.presentation.ui.MovieListUiState
+import com.devspacecinenow.list.presentation.ui.MovieUiData
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class MovieListViewModelTestEXSEMTURBINE {
+
+    private val repository: MovieListRepository = mock() //mock = dado mockado, é um dublê.
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+
+    private val testDispatcher = UnconfinedTestDispatcher(TestCoroutineScheduler())
+
+    private val underTest by lazy {
+        MovieListViewModel(repository, testDispatcher)
+    }
+
+
+
+    //Essa é a função, e seja feliz.
+    @Test
+    fun `Given fresh viewModel when collecting to nowPlaying then assert expected value`() {
+        runTest {
+            val movies = listOf(
+                Movie(
+                    id = 1,
+                    title = "title1",
+                    overview = "1",
+                    image = "image1",
+                    category = MovieCategory.NowPlaying.name,
+                    page = 1
+                )
+            )
+            whenever(repository.getNowPlaying(1)).thenReturn(Result.success(movies))
+
+            var result: MovieListUiState? = null
+
+            backgroundScope.launch(testDispatcher) {
+                result = underTest.uiNowPlaying.drop(1)
+                    .first() //aqui está pegando somente o segundo cenário.
+            }
+            val expected = MovieListUiState(
+                list = listOf(
+                    MovieUiData(
+                        id = 1, title = "title1", overview = "1", image = "image1"
+                    )
+                )
+            )
+            assertEquals(expected, result)
+        }
+    }
+}
