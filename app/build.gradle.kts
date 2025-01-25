b21771a4c6381f5f6c43ec82d5865806a7952639
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2025-01-25 17:30:35 -0300

    Studying

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("junit:junit:4.12")
 
 
     //Room database
